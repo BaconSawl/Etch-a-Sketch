2d32df38134dd13d741b6d72c5ff4b38e4f9d329
BaconSawl <ngolongvu306@gmail.com>
2026-03-18 15:31:57 +0700

    Add buttons to create new sketch and reset sketch The sketch is now fully functional! Just need to make it looks good.

--- javascript.js
+++ javascript.js
@@ -1,14 +1,16 @@
 const container = document.getElementById("container");
-const btn = document.getElementById("newGrid");
+const btnNewGrid = document.getElementById("newGrid");
+const btnResetGrid = document.getElementById("reset");
 
 function createGrid(size){
+    container.innerHTML = "";
     squareSize = 640 / size;
-    for (i = 0; i < size; i++){
-        for (j = 1; j <= size; j++){
+    for (let i = 0; i < size; i++){
+        for (let j = 1; j <= size; j++){
             const newDiv = document.createElement("div");
             newDiv.classList.add("square");
-            newDiv.style.width = squareSize;
-            newDiv.style.height = squareSize;
+            newDiv.style.width = squareSize + "px";
+            newDiv.style.height = squareSize + "px";
             newDiv.addEventListener("mouseover", (event) => {
                 event.target.style.backgroundColor = "black";
             })
@@ -17,19 +19,21 @@ function createGrid(size){
     }
 }
 
-createGrid(16);
-
-btn.addEventListener("click", () => {
-    /*
+btnNewGrid.addEventListener("click", () => {
     const x = Number(prompt("How many squares ?"));
     console.log(x);
     if (!(x >= 1 && x <= 100)){
         alert("invalid value");
     }
-    else */ {
-        container.innerHTML = ""; // Delete everything inside containerW
-        createGrid(16);
+    else {
+        container.innerHTML = ""; // Delete everything inside container
+        createGrid(x);
     }
+    
+    btnResetGrid.addEventListener("click", () => {
+        createGrid(x);
+    })
 })
+
 
 console.log(container.childNodes.length)
