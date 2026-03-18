c469256162fd41e79ded0e718d4848158d48a15c
BaconSawl <ngolongvu306@gmail.com>
2026-03-18 15:10:21 +0700

    Add "Reset" button This button is a placeholder, will replace with button and create new grid entirely.

--- javascript.js
+++ javascript.js
@@ -1,10 +1,14 @@
 const container = document.getElementById("container");
+const btn = document.getElementById("newGrid");
 
 function createGrid(size){
+    squareSize = 640 / size;
     for (i = 0; i < size; i++){
         for (j = 1; j <= size; j++){
             const newDiv = document.createElement("div");
             newDiv.classList.add("square");
+            newDiv.style.width = squareSize;
+            newDiv.style.height = squareSize;
             newDiv.addEventListener("mouseover", (event) => {
                 event.target.style.backgroundColor = "black";
             })
@@ -15,5 +19,17 @@ function createGrid(size){
 
 createGrid(16);
 
+btn.addEventListener("click", () => {
+    /*
+    const x = Number(prompt("How many squares ?"));
+    console.log(x);
+    if (!(x >= 1 && x <= 100)){
+        alert("invalid value");
+    }
+    else */ {
+        container.innerHTML = ""; // Delete everything inside containerW
+        createGrid(16);
+    }
+})
 
 console.log(container.childNodes.length)
